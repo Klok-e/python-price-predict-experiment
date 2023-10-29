dd1d0cc5e63c3b8613eefefb10076692c696fcad
LokiVKlokeNaAndoke <dmytro.sokolenko@tutanota.com>
2023-10-29 19:25:52 +0200

    00

--- custom_feature_extractor.py
+++ custom_feature_extractor.py
@@ -1,17 +1,18 @@
 from gymnasium import spaces
+from stable_baselines3.common.preprocessing import get_flattened_obs_dim
 from stable_baselines3.common.torch_layers import BaseFeaturesExtractor
 from torch import nn
 import torch as th
 
 
-class CustomCNN(BaseFeaturesExtractor):
+class CustomExtractor(BaseFeaturesExtractor):
     """
     :param observation_space: (gym.Space)
     :param features_dim: (int) Number of features extracted.
         This corresponds to the number of unit for the last layer.
     """
 
-    def __init__(self, observation_space: spaces.Dict):
+    def __init__(self, observation_space: spaces.Dict, lstm_hidden_size=32, lstm_layers=1):
         super().__init__(observation_space, features_dim=1)
 
         extractors = {}
@@ -20,23 +21,14 @@ class CustomCNN(BaseFeaturesExtractor):
         # so go over all the spaces and compute output feature sizes
         for key, subspace in observation_space.spaces.items():
             if key == "prices_sequence":
-                # Assuming the image is single-channel, and the shape is (C, H, W)
-                n_input_channels = subspace.shape[0]
-                self.extractors[key] = nn.Sequential(
-                    nn.Conv2d(n_input_channels, 32, kernel_size=8, stride=4, padding=0),
-                    nn.ReLU(),
-                    nn.Conv2d(32, 64, kernel_size=4, stride=2, padding=0),
-                    nn.ReLU(),
-                    nn.Flatten(),
-                )
-                # Compute the output shape by running a dummy input through the CNN
-                with th.no_grad():
-                    n_flatten = self.extractors[key](th.as_tensor(subspace.sample()[None], dtype=th.float32)).shape[1]
-                total_concat_size += n_flatten
+                # Assuming sequence length is `seq_len` and feature number is `n_features`
+                seq_len, n_features = subspace.shape
+                extractors[key] = nn.LSTM(input_size=n_features, hidden_size=lstm_hidden_size, batch_first=True,
+                                          num_layers=lstm_layers)
+                total_concat_size += lstm_hidden_size  # hidden_dim is the LSTM output size
             elif key == "other":
-                # Run through a simple MLP
-                extractors[key] = nn.Linear(subspace.shape[0], 16)
-                total_concat_size += 16
+                extractors[key] = nn.Flatten()
+                total_concat_size += get_flattened_obs_dim(subspace)
 
         self.extractors = nn.ModuleDict(extractors)
 
@@ -46,8 +38,15 @@ class CustomCNN(BaseFeaturesExtractor):
     def forward(self, observations: th.Tensor) -> th.Tensor:
         encoded_tensor_list = []
 
-        # self.extractors contain nn.Modules that do all the processing.
         for key, extractor in self.extractors.items():
-            encoded_tensor_list.append(extractor(observations[key]))
-        # Return a (B, self._features_dim) PyTorch tensor, where B is batch dimension.
+            obs_data = observations[key]
+            if key == "prices_sequence":
+                # LSTM expects input of shape (batch, seq_len, input_size)
+                obs_data = obs_data.view(obs_data.size(0), -1, extractor.input_size)  # reshape if necessary
+                out, _ = extractor(obs_data)
+                # Using only the last output tensor (hidden state)
+                encoded_tensor_list.append(out[:, -1, :])
+            else:
+                encoded_tensor_list.append(extractor(obs_data))
+
         return th.cat(encoded_tensor_list, dim=1)
