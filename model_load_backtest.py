18f51459a013a0e7a3a15efcd8dea0dbee012213
LokiVKlokeNaAndoke <dmytro.sokolenko@tutanota.com>
2023-12-30 21:20:39 +0200

    upd date

--- model_load_backtest.py
+++ model_load_backtest.py
@@ -10,7 +10,7 @@ from trading_metrics import calculate_metrics
 from util import download_and_process_data_if_available
 
 # rl_model = PPO.load("cherry-picked-best-models/rl-model-best1.pt")
-rl_model = PPO.load("cherry-picked-best-models/hs128_lstm1_net[256, 256]_ws64_150000_steps.zip")
+rl_model = PPO.load("cherry-picked-best-models/best_model.zip")
 
 df_tickers = download_and_process_data_if_available("cache/df_tickers.pkl")
 
@@ -22,7 +22,7 @@ for _, df, scaler, name in df_tickers:
 
     model_in_observations = 64
     skip_steps = 1024 + model_in_observations
-    bt = create_backtest_model_with_data(rl_model, df, scaler, "2023-5-1", "2023-5-14", model_in_observations)
+    bt = create_backtest_model_with_data(rl_model, df, scaler, "2023-11-01", "2023-11-14", model_in_observations)
     stats = bt.run()
     equity = bt._results._equity_curve["Equity"].iloc[skip_steps:]
     y = (equity - 1_000_000) / 1_000_000
