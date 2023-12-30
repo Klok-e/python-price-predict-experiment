0f19b2c78d63a217ab6f82f34e8506400b165750
LokiVKlokeNaAndoke <dmytro.sokolenko@tutanota.com>
2023-12-30 20:03:49 +0200

    1

--- train_model.py
+++ train_model.py
@@ -45,7 +45,7 @@ def train_model(df_tickers, hidden_size: int, lstm_layers: int, net_arch: list[i
 
     checkpoint_callback = CheckpointCallback(
         save_freq=max(100_000 // n_envs, 1),
-        save_path=f"{model_save_dir}-model/checkpoints/",
+        save_path=f"{model_save_dir}/checkpoints/",
         name_prefix=model_save_dir,
         verbose=1,
         save_vecnormalize=True,
