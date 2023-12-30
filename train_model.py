c14476b9ab9dfa796c76322d80bd82bb72ba7b6c
LokiVKlokeNaAndoke <dmytro.sokolenko@tutanota.com>
2023-12-30 19:40:30 +0200

    1

--- train_model.py
+++ train_model.py
@@ -58,7 +58,7 @@ def train_model(df_tickers, hidden_size: int, lstm_layers: int, net_arch: list[i
                             seed=42,
                             vec_env_cls=SubprocVecEnv)
     eval_callback = EvalCallback(eval_env,
-                                 best_model_save_path=f"{model_save_dir}/best-model/best_model",
+                                 best_model_save_path=f"{model_save_dir}/best-model",
                                  log_path=f"{model_save_dir}/best-model/results",
                                  eval_freq=max(100_000 // n_envs, 1), verbose=1,
                                  n_eval_episodes=4)
