9fa699b97162ca98d2daaecd60e419f55119971a
LokiVKlokeNaAndoke <dmytro.sokolenko@tutanota.com>
2023-12-30 17:57:11 +0200

    123

--- train_model.py
+++ train_model.py
@@ -14,7 +14,7 @@ from util import download_and_process_data_if_available
 
 @profile
 def train_model(df_tickers, hidden_size: int, lstm_layers: int, net_arch: list[int], timesteps: int,
-                model_window_size: int):
+                model_window_size: int, n_envs: int):
     model_save_dir = f"rl-model/hs{hidden_size}_lstm{lstm_layers}_net{net_arch}_ws{model_window_size}"
 
     split_date = '2023-11-01'
@@ -23,8 +23,6 @@ def train_model(df_tickers, hidden_size: int, lstm_layers: int, net_arch: list[i
         map(lambda ticker: (ticker[0].loc[:split_date], ticker[1].loc[:split_date], ticker[2], ticker[3]), df_tickers))
     df_tickers_test = list(
         map(lambda ticker: (ticker[0].loc[split_date:], ticker[1].loc[split_date:], ticker[2], ticker[3]), df_tickers))
-
-    n_envs = 5
 
     env = make_vec_env(CustomEnv, env_kwargs={"df_tickers": df_tickers_train,
                                               "model_in_observations": model_window_size,
@@ -80,4 +78,4 @@ if __name__ == "__main__":
             for arch in arch_list:
                 for window_size in window_size_list:
                     print(f"hidden_size {hidden_size}, lstm_layers {lstm_layers}, window_size {window_size}")
-                    train_model(df_tickers, hidden_size, lstm_layers, arch, 2_000_000, window_size)
+                    train_model(df_tickers, hidden_size, lstm_layers, arch, 2_000_000, window_size, 5)
