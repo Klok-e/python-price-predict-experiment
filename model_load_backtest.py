882827780ed959dbdd4403e89ccd48eef8e007c6
LokiVKlokeNaAndoke <dmytro.sokolenko@tutanota.com>
2023-12-31 14:57:59 +0200

    make it buy and sell (reward function)

--- model_load_backtest.py
+++ model_load_backtest.py
@@ -12,7 +12,7 @@ from util import download_and_process_data_if_available
 # rl_model = PPO.load("cherry-picked-best-models/rl-model-best1.pt")
 # rl_model = PPO.load("cherry-picked-best-models/best_model1.zip")
 # rl_model = PPO.load("cherry-picked-best-models/best_model.zip")
-rl_model = PPO.load("cherry-picked-best-models/hs128_lstm1_net[256, 256]_ws32_650000_steps.zip")
+rl_model = PPO.load("cherry-picked-best-models/hs128_lstm1_net[256, 256, 256]_ws128_500000_steps.zip")
 
 df_tickers = download_and_process_data_if_available("cache/df_tickers.pkl")
 
@@ -22,7 +22,7 @@ for _, df, scaler, name in df_tickers:
     t = time.time()
     print(f"backtest for {name} started")
 
-    model_in_observations = 32
+    model_in_observations = 128
     skip_steps = 1024 + model_in_observations
     bt = create_backtest_model_with_data(rl_model, df, scaler, "2023-09-01", "2023-10-01", model_in_observations)
     stats = bt.run()
