f66bbede12647e3a3cb4a366ef2cfe0570e54a6d
LokiVKlokeNaAndoke <dmytro.sokolenko@tutanota.com>
2023-12-31 21:55:24 +0200

    coin

--- util.py
+++ util.py
@@ -18,7 +18,7 @@ OBS_OTHER = "other"
 OBS_PRICES_SEQUENCE = "prices_sequence"
 
 TICKERS = [
-    "NEARUSDT", "SOLUSDT", "BTCUSDT", "ETHUSDT", "MOVRUSDT"
+    "NEARUSDT", "SOLUSDT", "BTCUSDT", "ETHUSDT", "BNBUSDT"
 ]
 
 
