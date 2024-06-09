f6261e6eeb3c00c491ec01d0c48807236474e4be
LokiVKlokeNaAndoke <dmytro.sokolenko@tutanota.com>
2024-06-09 16:42:17 +0300

    restore chackpoints

--- train_model.py
+++ train_model.py
@@ -13,51 +13,82 @@ count = 0
 
 
 # @profile
-def train_model(df_tickers_train, df_tickers_test, net_arch: list[int], timesteps: int,
-                model_window_size: int, n_envs: int, directory: str, model_save_name, policy_kwargs: dict):
+def train_model(
+    df_tickers_train,
+    df_tickers_test,
+    net_arch: list[int],
+    timesteps: int,
+    model_window_size: int,
+    n_envs: int,
+    directory: str,
+    model_save_name,
+    policy_kwargs: dict,
+):
     verify_custom_env(df_tickers_train)
 
-    env = make_vec_env(CustomEnv, env_kwargs={"df_tickers": df_tickers_train,
-                                              "model_in_observations": model_window_size},
-                       n_envs=n_envs, seed=42, vec_env_cls=SubprocVecEnv)
+    env = make_vec_env(
+        CustomEnv,
+        env_kwargs={
+            "df_tickers": df_tickers_train,
+            "model_in_observations": model_window_size,
+        },
+        n_envs=n_envs,
+        seed=42,
+        vec_env_cls=SubprocVecEnv,
+    )
 
-    policy_kvargs = dict(activation_fn=torch.nn.LeakyReLU,
-                         net_arch=net_arch,
-                         **policy_kwargs)
+    policy_kvargs = dict(
+        activation_fn=torch.nn.LeakyReLU, net_arch=net_arch, **policy_kwargs
+    )
 
     # {'gamma': 0.8, 'ent_coef': 0.02, 'gae_lambda': 0.92}
-    rl_model = PPO("MultiInputPolicy", env,
-                   verbose=1,
-                   tensorboard_log=f"{directory}/tensorboard/",
-                   ent_coef=0.02,
-                   gae_lambda=0.92,
-                   gamma=0.9,
-                   policy_kwargs=policy_kvargs,
-                   batch_size=1024,
-                   seed=42)
+    rl_model = PPO(
+        "MultiInputPolicy",
+        env,
+        verbose=1,
+        tensorboard_log=f"{directory}/tensorboard/",
+        ent_coef=0.02,
+        gae_lambda=0.92,
+        gamma=0.9,
+        policy_kwargs=policy_kvargs,
+        batch_size=1024,
+        seed=42,
+    )
     print(rl_model.policy)
 
-    # checkpoint_callback = CheckpointCallback(
-    #     save_freq=max(50_000 // n_envs, 1),
-    #     save_path=f"{directory}/rl-model/{model_save_name}/checkpoints/",
-    #     name_prefix=model_save_name,
-    #     verbose=1,
-    #     save_vecnormalize=True,
-    #     save_replay_buffer=True
-    # )
-    eval_env = make_vec_env(CustomEnv, env_kwargs={"df_tickers": df_tickers_test,
-                                                   "model_in_observations": model_window_size,
-                                                   "episodes_max": 10},
-                            seed=42,
-                            vec_env_cls=DummyVecEnv)
-    eval_callback = EvalCallback(eval_env,
-                                 best_model_save_path=f"{directory}/rl-model/{model_save_name}/best-model",
-                                 log_path=f"{directory}/rl-model/{model_save_name}/best-model/results",
-                                 eval_freq=max(100_000 // n_envs, 1), verbose=1,
-                                 n_eval_episodes=10)
+    checkpoint_callback = CheckpointCallback(
+        save_freq=max(50_000 // n_envs, 1),
+        save_path=f"{directory}/rl-model/{model_save_name}/checkpoints/",
+        name_prefix=model_save_name,
+        verbose=1,
+        save_vecnormalize=True,
+        save_replay_buffer=True,
+    )
+    eval_env = make_vec_env(
+        CustomEnv,
+        env_kwargs={
+            "df_tickers": df_tickers_test,
+            "model_in_observations": model_window_size,
+            "episodes_max": 10,
+        },
+        seed=42,
+        vec_env_cls=DummyVecEnv,
+    )
+    eval_callback = EvalCallback(
+        eval_env,
+        best_model_save_path=f"{directory}/rl-model/{model_save_name}/best-model",
+        log_path=f"{directory}/rl-model/{model_save_name}/best-model/results",
+        eval_freq=max(100_000 // n_envs, 1),
+        verbose=1,
+        n_eval_episodes=10,
+    )
 
-    learn = rl_model.learn(total_timesteps=timesteps, callback=[eval_callback],  # callback=[checkpoint_callback, eval_callback],
-                           tb_log_name=model_save_name)
+    learn = rl_model.learn(
+        total_timesteps=timesteps,
+        callback=[eval_callback],
+        callback=[checkpoint_callback, eval_callback],
+        tb_log_name=model_save_name,
+    )
 
     env.unwrapped.close()
     eval_env.unwrapped.close()
@@ -93,9 +124,20 @@ if __name__ == "__main__":
             for arch in arch_list:
                 for window_size in window_size_list:
                     print(
-                        f"hidden_size {hidden_size}, lstm_layers {lstm_layers}, window_size {window_size}")
-                    train_model(df_tickers, arch, 500_000, window_size, 5, "data",
-                                f"hs{hidden_size}_lstm{lstm_layers}_net{arch}_ws{window_size}",
-                                dict(features_extractor_class=LSTMExtractor,
-                                     features_extractor_kwargs=dict(lstm_hidden_size=hidden_size,
-                                                                    lstm_layers=lstm_layers)))
+                        f"hidden_size {hidden_size}, lstm_layers {lstm_layers}, window_size {window_size}"
+                    )
+                    train_model(
+                        df_tickers,
+                        arch,
+                        500_000,
+                        window_size,
+                        5,
+                        "data",
+                        f"hs{hidden_size}_lstm{lstm_layers}_net{arch}_ws{window_size}",
+                        dict(
+                            features_extractor_class=LSTMExtractor,
+                            features_extractor_kwargs=dict(
+                                lstm_hidden_size=hidden_size, lstm_layers=lstm_layers
+                            ),
+                        ),
+                    )
