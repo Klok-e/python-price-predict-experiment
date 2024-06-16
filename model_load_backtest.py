d149fdeba192ffd3d561d9b764906276f29db4e6
LokiVKlokeNaAndoke <dmytro.sokolenko@tutanota.com>
2024-06-16 12:02:28 +0300

    fmt

--- model_load_backtest.py
+++ model_load_backtest.py
@@ -2,14 +2,18 @@ import time
 
 from trading_metrics import calculate_metrics
 from util import save_pickle
+import pandas as pd
 
 
-def run_backtest_on_all_tickers(df_tickers, strat_name, in_obs, create_strategy_func, computed_data_dir):
+def run_backtest_on_all_tickers(
+    df_tickers, strat_name, in_obs, create_strategy_func, computed_data_dir
+):
     sum_equity = None
     trades = 0
     for _, df, scaler, name in df_tickers:
-        start = "2024-02-01"
-        end = "2024-03-01"
+        df = df.read()
+        start = df.index.max() - pd.Timedelta(days=32)
+        end = df.index.max()
 
         t = time.time()
         print(f"backtest for {name} started")
@@ -18,7 +22,10 @@ def run_backtest_on_all_tickers(df_tickers, strat_name, in_obs, create_strategy_
         bt = create_strategy_func(df, scaler, in_obs, start, end)
         res = bt.run()
 
-        save_pickle((res._trades, res._equity_curve), f"{computed_data_dir}/backtest-results/{strat_name}_{name}.pkl")
+        save_pickle(
+            (res._trades, res._equity_curve),
+            f"{computed_data_dir}/backtest-results/{strat_name}_{name}.pkl",
+        )
 
         trades += len(res._trades)
 
@@ -34,9 +41,11 @@ def run_backtest_on_all_tickers(df_tickers, strat_name, in_obs, create_strategy_
     metrics = calculate_metrics(sum_equity, trades, start_cash)
     print()
     print(f"{strat_name} metrics:")
-    print(f"cumulative_return={metrics[0]:.4f}, "
-          f"max_earning_rate={metrics[1]:.4f}, "
-          f"maximum_pullback={metrics[2]:.4f}, "
-          f"average_profitability_per_trade={metrics[3]:.4f}, "
-          f"sharpe_ratio={metrics[4]:.4f}")
+    print(
+        f"cumulative_return={metrics[0]:.4f}, "
+        f"max_earning_rate={metrics[1]:.4f}, "
+        f"maximum_pullback={metrics[2]:.4f}, "
+        f"average_profitability_per_trade={metrics[3]:.4f}, "
+        f"sharpe_ratio={metrics[4]:.4f}"
+    )
     print()
