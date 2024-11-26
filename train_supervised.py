9d9383779544ca8f10ebbaf665baafe211b744b7
LokiVKlokeNaAndoke <dmytro.sokolenko@tutanota.com>
2024-11-26 23:31:24 +0200

    opt

--- train_supervised.py
+++ train_supervised.py
@@ -58,10 +58,10 @@ def train_supervised_model(model_type, model_kwargs, df_tickers_train, df_ticker
     train_dataset = PriceDataset(df_tickers_train, window_size)
     test_dataset = PriceDataset(df_tickers_test, window_size)
 
-    sample_weights_train = calculate_sample_weights(train_dataset, window_size)
+    sample_weights_train = calculate_sample_weights(df_tickers_train, window_size)
     sampler_train = WeightedRandomSampler(sample_weights_train, len(train_dataset))
 
-    sample_weights_test = calculate_sample_weights(test_dataset, window_size)
+    sample_weights_test = calculate_sample_weights(df_tickers_test, window_size)
     sampler_test = WeightedRandomSampler(sample_weights_test, len(test_dataset))
 
     # Create dataloaders
