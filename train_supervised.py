02142de6c4e50baa6f8ea01de991ab61e664defc
LokiVKlokeNaAndoke <dmytro.sokolenko@tutanota.com>
2024-11-28 00:02:16 +0200

    add test_prediction_threshold parameter

--- train_supervised.py
+++ train_supervised.py
@@ -59,7 +59,7 @@ def load_model(model, model_path):
 def train_supervised_model(model_type, model_kwargs, df_tickers_train, df_tickers_test, window_size,
                            computed_data_dir, model_name, epochs=10,
                            batch_size=4096, learning_rate=0.0001, log_interval=100, save_model=False,
-                           continue_training=True):
+                           continue_training=True, test_prediction_threshold=0.6):
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
     feature_size = df_tickers_train[0][0].shape[1]
@@ -131,7 +131,7 @@ def train_supervised_model(model_type, model_kwargs, df_tickers_train, df_ticker
                         total_test_loss += loss.item()
 
                         # Calculate predictions
-                        predictions = (outputs > 0.6).float()
+                        predictions = (outputs > test_prediction_threshold).float()
 
                         # Store labels, probabilities, and predictions
                         all_labels.extend(labels.cpu().numpy())
